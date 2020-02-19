26f3152eea1ed2607ad8ddb4acff14c04e395e96
SongSeoYoung <bgd216@naver.com>
2020-02-19 19:17:40 +0900

    using Component ref

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
-function App() {
-  return (
-    <div >
-      <ValidationSample/>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div >
+        <ScrollBox ref={(ref)=> this.ScrollBox=ref}/>
+        <button onClick={() => 
+          this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
 }
+  
 
 export default App;
