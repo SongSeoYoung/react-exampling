cad27134db3620e126256520a162dc0c4a359199
SongSeoYoung <bgd216@naver.com>
2020-02-23 16:48:50 +0900

    lifeCycleMethod

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import './App.css';
-import IterationSample from './IterationSample';
+import LifeCycleSmaple from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+
 
 class App extends Component{
+
+  state = {
+    color:'#00000'
+  }
+
+  handleClick = () =>{
+    this.setState({
+      color:getRandomColor()
+    })
+  }
   render() {
     return (
       <div >
-        <IterationSample/>
+        <button onClick = {this.handleClick}>랜덤색상 출력</button>
+        <LifeCycleSmaple color={this.state.color}/>
       </div>
     );
   }
