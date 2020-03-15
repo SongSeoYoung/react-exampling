a12cca1a2478276ca167f7fbe71f6e6961e46059
SongSeoYoung <bgd216@naver.com>
2020-03-15 18:09:23 +0900

    query

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import queryString from 'query-string'
 
-const About =({match}) => {
+const About =({match, location}) => {
+  const query = queryString.parse(location.search)
+  console.log(query)
   return (
     <div>
       <h2>About{match.params.name}</h2>
